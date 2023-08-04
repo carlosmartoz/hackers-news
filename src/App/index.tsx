9ad2e8ae73771468d7bcfad68ee85318f79638e1
Carlos Martinez <cmartinez@mcontigo.com>
2023-08-04 19:09:02 -0300

    update/wpi: storys page

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Route } from 'wouter'
+import { Suspense } from 'react'
 import { Header } from '../Components/Header'
+import TopStoriesPage from '../Page/ToptoriesPage'
 
 export default function App () {
   return (
     <>
       <Header />
-      <main>Hackers News</main>
+      <main>
+        <Suspense fallback={null}>
+          <Route path="/" component={TopStoriesPage} />
+        </Suspense>
+      </main>
     </>
   )
 }
